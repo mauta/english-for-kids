c690fb338855cf39298c76fe6bce400b65f5238b
mauta <anna.opareva@gmail.com>
2020-11-20 16:52:16 +0300

    feat: add json

--- english-for-kids/public/bundle.js
+++ english-for-kids/public/bundle.js
@@ -86,13 +86,186 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./src/block/extcontrol.js":
+/*!*********************************!*\
+  !*** ./src/block/extcontrol.js ***!
+  \*********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return ExtControl; });
+/* harmony import */ var _utils_control__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../utils/control */ "./src/utils/control.js");
+/* eslint-disable no-unused-expressions */
+
+
+class ExtControl extends _utils_control__WEBPACK_IMPORTED_MODULE_0__["default"] {
+  constructor(parentNode, className = '', className2 = '', content = '', onclick) {
+    super(parentNode, 'div', className, content);
+    this.isChecked = false;
+    this.className = className;
+    this.className2 = className2;
+    this.onClick = onclick;
+
+    this.node.onclick = () => {
+      this.changeState();
+      this.onClick && this.onClick(this.isChecked);
+    };
+  }
+
+  changeState(newState) {
+    this.isChecked = newState !== undefined ? newState : !this.isChecked;
+    this.node.className = this.isChecked ? this.className2 : this.className;
+  }
+}
+
+
+/***/ }),
+
+/***/ "./src/block/menu.js":
+/*!***************************!*\
+  !*** ./src/block/menu.js ***!
+  \***************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Menu; });
+/* harmony import */ var _utils_control__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../utils/control */ "./src/utils/control.js");
+/* harmony import */ var _extcontrol__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./extcontrol */ "./src/block/extcontrol.js");
+/* eslint-disable max-len */
+/* eslint-disable no-unused-expressions */
+
+
+
+class Menu extends _utils_control__WEBPACK_IMPORTED_MODULE_0__["default"] {
+  constructor(parentNode, className = '', itemClassName = '', itemClassNameSelected = '') {
+    super(parentNode, 'div', className, '');
+    this.className = className;
+    this.itemClassName = itemClassName;
+    this.itemClassNameSelected = itemClassNameSelected;
+
+    this.arr = [];
+    this.onChange = () => {};
+  }
+
+  addItem(content) {
+    const elem = new _extcontrol__WEBPACK_IMPORTED_MODULE_1__["default"](this.node, this.itemClassName, this.itemClassNameSelected, content, () => {
+      this.select(this.arr.indexOf(elem));
+    });
+    this.arr.push(elem);
+  }
+
+  select(ind) {
+    this.arr.forEach((item) => {
+      item.changeState(false);
+    });
+
+    this.arr[ind] && this.arr[ind].changeState(true);
+    this.onChange(ind, this.arr[ind]);
+  }
+}
+
+
+/***/ }),
+
 /***/ "./src/main.js":
 /*!*********************!*\
   !*** ./src/main.js ***!
   \*********************/
-/*! no static exports found */
-/***/ (function(module, exports) {
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
 
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _block_menu__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./block/menu */ "./src/block/menu.js");
+/* harmony import */ var _utils_control__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./utils/control */ "./src/utils/control.js");
+
+
+
+fetch('./data.json').then((res) => res.json()).then((json) => {
+  console.log(json);
+});
+
+const elem = new _block_menu__WEBPACK_IMPORTED_MODULE_0__["default"](document.body, '', 'cl cl5', 'cl cl6');
+elem.onChange = (ind) => {
+  location.hash = ind;
+};
+
+for (let i = 0; i < 10; i += 1) {
+  elem.addItem(i);
+}
+
+const el = new _utils_control__WEBPACK_IMPORTED_MODULE_1__["default"](document.body);
+
+window.onpopstate = () => {
+  switch (location.hash) {
+    case '#0':
+      el.node.innerHTML = 'test content 0';
+      break;
+    case '#1':
+      el.node.innerHTML = 'test content 1';
+      break;
+    case '#2':
+      el.node.innerHTML = 'test content 2';
+      break;
+    case '#3':
+      el.node.innerHTML = 'test content 3';
+      break;
+    case '#4':
+      el.node.innerHTML = 'test content 4';
+      break;
+    case '#5':
+      el.node.innerHTML = 'test content 5';
+      break;
+    case '#6':
+      el.node.innerHTML = 'test content 6';
+      break;
+    case '#7':
+      el.node.innerHTML = 'test content 7';
+      break;
+    case '#8':
+      el.node.innerHTML = 'test content 8';
+      break;
+    case '#9':
+      el.node.innerHTML = 'test content 9';
+      break;
+    default:
+      el.node.innerHTML = '';
+      break;
+  }
+
+  console.log(window.location);
+  const num = location.hash.slice(1);
+  elem.select(num);
+};
+
+location.hash ? window.onpopstate() : elem.select(0);
+
+
+
+/***/ }),
+
+/***/ "./src/utils/control.js":
+/*!******************************!*\
+  !*** ./src/utils/control.js ***!
+  \******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return Control; });
+class Control {
+  constructor(parentNode, tag = 'div', className = '', content = '') {
+    this.node = document.createElement(tag);
+    this.node.className = className;
+    this.node.innerHTML = content;
+    parentNode.appendChild(this.node);
+  }
+}
 
 
 /***/ })
--- english-for-kids/src/main.js
+++ english-for-kids/src/main.js
@@ -1,57 +1,67 @@
 import Menu from './block/menu';
 import Control from './utils/control';
 
-const elem = new Menu(document.body, '', 'cl cl5', 'cl cl6');
-elem.onChange = (ind) => {
-  location.hash = ind;
-};
+fetch('../assets/data.json').then((res) => res.json()).then((json) => {
+  const category = new Set();
 
-for (let i = 0; i < 10; i += 1) {
-  elem.addItem(i);
-}
-
-const el = new Control(document.body);
-
-window.onpopstate = () => {
-  switch (location.hash) {
-    case '#0':
-      el.node.innerHTML = 'test content 0';
-      break;
-    case '#1':
-      el.node.innerHTML = 'test content 1';
-      break;
-    case '#2':
-      el.node.innerHTML = 'test content 2';
-      break;
-    case '#3':
-      el.node.innerHTML = 'test content 3';
-      break;
-    case '#4':
-      el.node.innerHTML = 'test content 4';
-      break;
-    case '#5':
-      el.node.innerHTML = 'test content 5';
-      break;
-    case '#6':
-      el.node.innerHTML = 'test content 6';
-      break;
-    case '#7':
-      el.node.innerHTML = 'test content 7';
-      break;
-    case '#8':
-      el.node.innerHTML = 'test content 8';
-      break;
-    case '#9':
-      el.node.innerHTML = 'test content 9';
-      break;
-    default:
-      el.node.innerHTML = '';
-      break;
+  for (let i = 0; i < json.length; i += 1) {
+    category.add(json[i].category);
   }
 
-  console.log(window.location);
-  const num = location.hash.slice(1);
-  elem.select(num);
-};
+  const elem = new Menu(document.body, '', 'cl cl5', 'cl cl6');
 
-location.hash ? window.onpopstate() : elem.select(0);
+  elem.onChange = (ind) => {
+    location.hash = ind;
+  };
+
+  category.forEach((item) => elem.addItem(item));
+
+
+
+  const el = new Control(document.body);
+
+  window.onpopstate = () => {
+    switch (location.hash) {
+      case '#0':
+        el.node.innerHTML = 'test content 0';
+        break;
+      case '#1':
+        el.node.innerHTML = 'test content 1';
+        break;
+      case '#2':
+        el.node.innerHTML = 'test content 2';
+        break;
+      case '#3':
+        el.node.innerHTML = 'test content 3';
+        break;
+      case '#4':
+        el.node.innerHTML = 'test content 4';
+        break;
+      case '#5':
+        el.node.innerHTML = 'test content 5';
+        break;
+      case '#6':
+        el.node.innerHTML = 'test content 6';
+        break;
+      case '#7':
+        el.node.innerHTML = 'test content 7';
+        break;
+      case '#8':
+        el.node.innerHTML = 'test content 8';
+        break;
+      case '#9':
+        el.node.innerHTML = 'test content 9';
+        break;
+      default:
+        el.node.innerHTML = '';
+        break;
+    }
+
+    console.log(window.location);
+    const num = location.hash.slice(1);
+    elem.select(num);
+  };
+
+  location.hash ? window.onpopstate() : elem.select(0);
+
+});
